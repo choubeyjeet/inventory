1c1348352c09f5496335a99c7c8fccf9b41a9f5a
choubeyjeet <choubeyjeet2580@gmail.com>
2025-11-03 15:09:05 +0530

    Updated Dashboard

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://inventory-seven-omega.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
