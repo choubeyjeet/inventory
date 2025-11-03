f9a552ae4153914e311a74a25e29eb7589c4cc0b
choubeyjeet <choubeyjeet2580@gmail.com>
2025-11-03 14:55:49 +0530

    Added new Dashboard charts

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,10 @@
-export default {
-  server: {
-    watch: {
-      usePolling: true,
-    },
-  },
-};
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: './', // important when deploying on static hosts
+  build: {
+    outDir: 'dist'
+  }
+})
